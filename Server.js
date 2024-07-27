53eaaa50a213e7829513375e8eada831cc0f3a0d
Babun001 <babunroy987@gmail.com>
2024-07-27 12:31:17 +0530

    wtf

--- Server.js
+++ Server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const request = require('request');
 const port = 8080;
 const App = express()
 
 App.get("/api/boom",(req,res) =>{
     res.send("This is the first api you visit!");
 });
+
+App.get("/api/diabetes",(req,res) => {
+    request('http://localhost:5000/diabetes', (err,response) => {
+        console.error("error: ", err);
+        console.log("response: ", response);
+    })
+})
 
 App.listen(port, () =>{
     console.log(`Backend Server is running on port ${port}`);
--- flask_api.py
+++ flask_api.py
@@ -16,6 +16,19 @@ def welcome():
     return ("Welcome all!")
 
 
+@app.route('/diabetes', methods=['GET'])
+def Diabetes_prediction():
+    Pregnancies = request.args.get('Pregnancies')
+    Glucose = request.args.get('Glucose')
+    BloodPressure = request.args.get('BloodPressure')
+    Insulin = request.args.get('Insulin')
+    BMI = request.args.get('BMI')
+    DiabetesPedigreeFunction = request.args.get('DiabetesPedigreeFunction')
+    Age = request.args.get('Age')
+    predicted_value = model.predict([[Pregnancies,Glucose,BloodPressure,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+    return predicted_value
+
+
 
 if __name__ == '__main__':
     app.run()
